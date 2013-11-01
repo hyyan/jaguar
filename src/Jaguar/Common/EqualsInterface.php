78abba7d4116f62c8290d3b2b1d415253710df7a
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-01 02:49:25 -0700

    added equals interface

---
+++ src/Jaguar/Common/EqualsInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jaguar\Common;
+
+/**
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+interface EqualsInterface {
+
+    /**
+     * Check that the current object which implements this interfcae is equal
+     * to another object from the same type
+     * 
+     * @param mixed $other
+     * 
+     * @throws \InvalidArgumentException if the passed param type is invalid
+     */
+    public function equals($other);
+}
+
