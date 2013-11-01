675f834a1663f2aadb37a8b26518221819597db9
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-01 02:49:54 -0700

    added JaguarTestCase

---
+++ tests/Jaguar/Tests/JaguarTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Jaguar\Tests;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class JaguarTestCase extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Get fixture from fixtures folder
+     * 
+     * @param string $file
+     * @return string the full path for the fixture file
+     */
+    public function getFixture($file) {
+        return __DIR__ . '/../Fixtures/' . $file;
+    }
+
+}
+
