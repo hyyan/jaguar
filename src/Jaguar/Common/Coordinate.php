b976c70d5b7d89764ebbf64fbdfabdf7a3d87236
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-01 02:50:15 -0700

    added coordinate object and passed the tests

---
+++ src/Jaguar/Common/Coordinate.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Jaguar\Common;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Coordinate implements EqualsInterface {
+
+    private $x;
+    private $y;
+
+    /**
+     * Construct New Coordinate Object
+     * 
+     * @param integer $x x pos
+     * @param integer $y y pos
+     */
+    public function __construct($x = 0, $y = 0) {
+        $this->move($x, $y);
+    }
+
+    /**
+     * Set X Position
+     * 
+     * @param integer $x
+     * 
+     * @return \Jaguar\Common\Coordinate
+     */
+    public function setX($x) {
+        $this->x = (integer) floor($x);
+        return $this;
+    }
+
+    /**
+     * Get X Position
+     * 
+     * @return integer
+     */
+    public function getX() {
+        return $this->x;
+    }
+
+    /**
+     * Set Y Position
+     * 
+     * @param integer $y
+     * 
+     * @return \Jaguar\Common\Coordinate
+     */
+    public function setY($y) {
+        $this->y = (integer) floor($y);
+        return $this;
+    }
+
+    /**
+     * Get Y position
+     * 
+     * @return integer
+     */
+    public function getY() {
+        return $this->y;
+    }
+
+    /**
+     * Chage the current coordinate location to match the passed coordinate 
+     * location
+     * 
+     * @param \Jaguar\Common\Coordinate $coordinate 
+     * 
+     * @return \Jaguar\Common\Coordinate
+     */
+    public function setLocation(Coordinate $coordinate) {
+        return $this->setX($coordinate->getX())->setY($coordinate->getY());
+    }
+
+    /**
+     * Get New Coordinate Object with same location of the current one
+     * 
+     * @return \Jaguar\Common\Coordinate
+     */
+    public function getLocation() {
+        return new Coordinate($this->getX(), $this->getY());
+    }
+
+    /**
+     * Moves this coordinate to the specified location in the (x,y) 
+     * coordinate plane.
+     *  
+     * @param integer $x
+     * @param integer $y
+     * 
+     * @return \Jaguar\Common\Coordinate
+     */
+    public function move($x, $y) {
+        return $this->setX($x)->setY($y);
+    }
+
+    /**
+     * Translates this point, at location (x,y),
+     * by {dx} along the {x} axis and {dy}
+     * along the {y} axis so that it now represents the coordinate
+     * {x+dx,y+dy)}.
+     * 
+     * @param integer $x the distance to move this coordinate
+     *                   along the X axis
+     * @param integer $y the distance to move this coordinate
+     *                   along the Y axis
+     * 
+     * @return \Jaguar\Common\Coordinate
+     */
+    public function translate($dx, $dy) {
+        return $this->setX($dx + $this->getX())->setY($dy + $this->getY());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function equals($other) {
+
+        if (!($other instanceof self)) {
+            throw new \InvalidArgumentException('Invalid Coordinate Object');
+        }
+
+        if (
+                ($other->getX() == $this->getX()) &&
+                ($other->getY() == $this->getY())
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns a string representation of the current coordinate object
+     * 
+     * @return string
+     */
+    public function __toString() {
+        return get_called_class()
+                . "["
+                    . "x={$this->getX()},"
+                    . "y={$this->getY()}"
+                . "]";
+    }
+
+}
+
---
+++ tests/Jaguar/Tests/Common/CoordinateTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Jaguar\Tests\Common;
+
+use Jaguar\Tests\JaguarTestCase;
+use Jaguar\Common\Coordinate;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class CoordinateTest extends JaguarTestCase {
+
+    public function testEqualsThrowInvalidArgumentException() {
+        $this->setExpectedException('\InvalidArgumentException');
+        $c = new Coordinate();
+        $c->equals('invalid');
+    }
+
+    public function testEquals() {
+        $c = new Coordinate();
+        $this->assertTrue($c->equals(clone $c));
+        $this->assertFalse($c->equals(new Coordinate(10, 10)));
+    }
+
+    public function testToString() {
+        $this->assertInternalType('string', (string) new Coordinate());
+    }
+
+    public function testTranslate() {
+        $c = new Coordinate();
+        $c->translate(50, 50);
+
+        $this->assertTrue($c->equals(new Coordinate(50, 50)));
+    }
+
+    public function testLocation() {
+        $c = new Coordinate();
+        $newLocation = new Coordinate(50, 50);
+
+        $c->setLocation($newLocation);
+        $getNewLocation = $c->getLocation(); // new coordinate object
+
+        $this->assertNotSame($getNewLocation, $newLocation);
+        $this->assertTrue($getNewLocation->equals($newLocation));
+    }
+
+}
+
