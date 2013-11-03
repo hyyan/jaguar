3beae4903d9be5ee908a92e87cd4aaa4627473cf
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-02 20:10:36 -0700

    Added Tiled Color

---
+++ src/Jaguar/Color/TiledColor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Jaguar\Color;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class TiledColor extends AbstractColor {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function doEquals(ColorInterface $color) {
+        return $color->getValue() === IMG_COLOR_TILED;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getValue() {
+        return IMG_COLOR_TILED;
+    }
+
+}
+
---
+++ tests/Jaguar/Tests/Color/TiledColorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Jaguar\Tests\Color;
+
+use Jaguar\Color\TiledColor;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class TiledColorTest extends AbstractColorTest {
+
+    public function getColor() {
+        return new TiledColor();
+    }
+
+    public function testEquals() {
+        $this->assertTrue($this->getColor()->equals($this->getColor()));
+    }
+
+}
+
