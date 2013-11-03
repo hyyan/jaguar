f71336f1613705f56ea5d69c463ed9cf8eb52998
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-02 18:45:58 -0700

    Added PalleteConvertException

---
+++ src/Jaguar/Exception/Canvas/PalleteConvertException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jaguar\Execption\Canvas;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class PalleteConvertException extends CanvasException {
+    
+    public function __construct(
+    $message='Unbale To Convert Pallete To TrueColor', $code=null, $previous=null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
+
