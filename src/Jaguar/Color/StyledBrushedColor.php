e10a0f5d3e66a48090a6259644139421e4a9b3be
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-02 20:19:59 -0700

    Added StyledBrushed Color

---
+++ src/Jaguar/Color/StyledBrushedColor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Jaguar\Color;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class StyledBrushedColor extends AbstractColor{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function doEquals(ColorInterface $color) {
+        return $color->getValue() === IMG_COLOR_STYLEDBRUSHED;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getValue() {
+        return IMG_COLOR_STYLEDBRUSHED;
+    }
+
+}
+
