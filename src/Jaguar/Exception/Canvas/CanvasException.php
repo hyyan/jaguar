524bfdacffae508a854be235520fc34448e3c29c
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-02 22:01:29 -0700

    Added Canvas Exceptions

---
+++ src/Jaguar/Exception/Canvas/CanvasException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jaguar\Execption\Canvas;
+
+use Jaguar\Execption\JaguarException;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class CanvasException extends JaguarException {
+    
+}
+
