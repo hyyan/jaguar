b583b927fcdaa4729b649f57adcd394f04ae1d00
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-02 20:15:31 -0700

    Added Styled Color

---
+++ tests/Jaguar/Tests/Color/StyledColorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Jaguar\Tests\Color;
+
+use Jaguar\Color\StyledColor;
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class StyledColorTest extends AbstractColorTest {
+
+    public function getColor() {
+        return new StyledColor();
+    }
+
+    public function testEquals() {
+        $this->assertTrue($this->getColor()->equals($this->getColor()));
+    }
+
+}
+
