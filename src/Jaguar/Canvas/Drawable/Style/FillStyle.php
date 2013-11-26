dd7378481e3136274357a88bd85fb4845bed28ad
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-11-26 07:49:06 -0800

    Added FillStyle

---
+++ src/Jaguar/Canvas/Drawable/Style/FillStyle.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Canvas\Drawable\Style;
+
+use Jaguar\Canvas\Drawable\StyleInterface;
+use Jaguar\Canvas\CanvasInterface;
+use Jaguar\Canvas\Drawable\DrawableInterface;
+use Jaguar\Exception\Canvas\Drawable\DrawableException;
+use Jaguar\Color\TiledColor;
+
+class FillStyle implements StyleInterface {
+
+    private $pattern;
+
+    /**
+     * Constrcut new PatternFill Style
+     * 
+     * @param \Jaguar\Canvas\CanvasInterface $pattern
+     */
+    public function __construct(CanvasInterface $pattern) {
+        $this->pattern = $pattern;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(CanvasInterface $canvas, DrawableInterface $drawable) {
+        if (
+                false == @imagesettile(
+                        $canvas->getHandler()
+                        , $this->pattern->getHandler()
+                )
+        ) {
+            throw new DrawableException('Could Not Apply The Fill Pattern Style');
+        }
+        return new TiledColor();
+    }
+
+    /**
+     * Disable Style Clone
+     * 
+     * @throws \RuntimeException
+     */
+    public function __clone() {
+        throw new \RuntimeException('Clone Is Not Possible ON PatterFill Style');
+    }
+
+}
+
---
+++ tests/Jaguar/Tests/Canvas/Drawable/Style/FillStyleTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Jaguar package.
+ *
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Tests\Canvas\Drawable\Style;
+
+use Jaguar\Tests\Canvas\Drawable\AbstractStyleTest;
+use Jaguar\Canvas\Drawable\Style\FillStyle;
+
+class FillStyleTest extends AbstractStyleTest {
+
+    public function testApply() {
+        $style = new FillStyle($this->getCanvas());
+        $this->assertInstanceOf(
+                '\Jaguar\Color\TiledColor'
+                , $style->apply($this->getCanvas(), $this->getDrawable())
+        );
+    }
+
+    /**
+     * @expectedException \Jaguar\Exception\Canvas\Drawable\DrawableException
+     */
+    public function testApplyThrowDrawableException() {
+        $style = new FillStyle(new \Jaguar\Tests\Canvas\Mock\CanvasMock());
+        $style->apply($this->getCanvas(), $this->getDrawable());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testCloneThrowRuntimeException() {
+        $style = new FillStyle($this->getCanvas());
+        clone $style;
+    }
+
+}
+
