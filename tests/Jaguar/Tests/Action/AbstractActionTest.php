594ef41a3a6d82f4c9e7fb8562c2156ab1cf2418
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-12-05 11:56:29 -0800

    Added AbstractAction

---
+++ tests/Jaguar/Tests/Action/AbstractActionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Jaguar package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Tests\Action;
+
+use Jaguar\Tests\JaguarTestCase;
+
+abstract class AbstractActionTest extends JaguarTestCase
+{
+
+    /**
+     * Get an action instance
+     * 
+     * @return \Jaguar\Action\ActionInterface 
+     */
+    abstract public function getAction();
+
+    /**
+     * Get canvas instance
+     * 
+     * @return \Jaguar\Canvas
+     */
+    public function getCanvas()
+    {
+        return new \Jaguar\Canvas(new \Jaguar\Dimension(100, 100));
+    }
+
+    /**
+     * @expectedException \Jaguar\Exception\CanvasEmptyException
+     */
+    public function testApplyThrowCanvasEmptyException()
+    {
+        $this->getAction()->apply(new \Jaguar\Tests\Mock\EmptyCanvasMock());
+    }
+
+    public function testApply()
+    {
+        $this->assertInstanceOf(
+                '\Jaguar\Action\ActionInterface'
+                , $this->getAction()->apply($this->getCanvas())
+        );
+    }
+
+}
