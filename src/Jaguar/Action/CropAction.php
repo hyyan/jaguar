8eee6004359e6e4feef73435b67b286b5f9ec654
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-12-05 11:56:41 -0800

    Added Crop Action

---
+++ src/Jaguar/Action/CropAction.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the Jaguar package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Action;
+
+use Jaguar\CanvasInterface;
+use Jaguar\Box;
+use Jaguar\Canvas;
+use Jaguar\Dimension;
+
+class CropAction extends AbstractAction
+{
+    private $box;
+
+    /**
+     * Constrcut new crop action
+     * 
+     * @param \Jaguar\Box $box
+     */
+    public function __construct(Box $box = null)
+    {
+        $this->setBox($box === null ? new Box(new Dimension(1, 1)) : $box);
+    }
+
+    /**
+     * Set crop box
+     * 
+     * @param \Jaguar\Box $box
+     * 
+     * @return \Jaguar\Action\CropAction
+     */
+    public function setBox(Box $box)
+    {
+        $this->box = $box;
+        return $this;
+    }
+
+    /**
+     * Get crop box
+     * 
+     * @return \Jaguar\Box
+     */
+    public function getBox()
+    {
+        return $this->box;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function doApply(CanvasInterface $canvas)
+    {
+        $dimension = $this->getBox()->getDimension();
+        $coordinate = $this->getBox()->getCoordinate();
+
+        $new = new Canvas($dimension, Canvas::Format_PNG);
+        $new->create($dimension);
+        $new->paste($canvas, new Box($dimension, $coordinate), new Box($dimension));
+        $canvas->fromCanvas($new);
+        $new->destroy();
+    }
+
+}
---
+++ tests/Jaguar/Tests/Action/CropActionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Jaguar package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Tests\Action;
+
+use Jaguar\Action\CropAction;
+
+class CropActionTest extends AbstractActionTest
+{
+
+    public function getAction()
+    {
+        return new CropAction();
+    }
+
+}
