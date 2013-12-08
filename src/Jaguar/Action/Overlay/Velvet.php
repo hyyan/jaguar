61ebdd7c0d3284605a65912859119dfdbcdb0416
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-12-08 12:40:40 -0800

    Added Velvet

---
+++ src/Jaguar/Action/Overlay/Velvet.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Jaguar package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Action\Overlay;
+
+use Jaguar\Action\Color\Brightness;
+use Jaguar\Action\Color\Contrast;
+use Jaguar\Action\Color\Colorize;
+use Jaguar\Action\Overlay;
+use Jaguar\Color\RGBColor;
+
+class Velvet extends AbstractOverlay
+{
+
+    /**
+     * {@inheritdoc}
+     *
+     * this effect was inspired from Marc Hibbins (http://marchibbins.com/dev/gd)
+     */
+    protected function doApply(\Jaguar\CanvasInterface $canvas)
+    {
+        $actions = array(
+            new Brightness(5),
+            new Contrast(25),
+            new Colorize(new RGBColor(0, 45, 65)),
+            new Overlay(
+                    $this->getOverlayCanvas('noise.png'), 45
+            ),
+            new Overlay(
+                    $this->getOverlayCanvas('vignette.png'), 100
+            )
+        );
+
+        foreach ($actions as $action) {
+            $action->apply($canvas);
+        }
+    }
+
+}
