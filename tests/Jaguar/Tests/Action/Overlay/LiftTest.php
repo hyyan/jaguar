357e2f08c8d44bdc24d8a8ebc374edfc6d2571aa
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-12-08 12:53:35 -0800

    Added Lift

---
+++ tests/Jaguar/Tests/Action/Overlay/LiftTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Jaguar package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jaguar\Tests\Action\Overlay;
+
+use Jaguar\Action\Overlay\Lift;
+use Jaguar\Tests\Action\AbstractActionTest;
+
+class LiftTest extends AbstractActionTest
+{
+
+    public function getAction()
+    {
+        return new Lift();
+    }
+
+    public function testApply()
+    {
+        $this->assertInstanceOf(
+                '\Jaguar\Action\Overlay\Lift'
+                , $this->getAction()->apply($this->getCanvas())
+        );
+    }
+
+}
