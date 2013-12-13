7bce07d53a320229cf666b23a3289764da78ba9d
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2013-12-13 10:08:57 -0800

    added new sharpen types for sharpen action

--- src/Jaguar/Action/Sharpen.php
+++ src/Jaguar/Action/Sharpen.php
@@ -14,17 +14,82 @@ use Jaguar\CanvasInterface;
 
 class Sharpen extends AbstractAction
 {
+    const SHARPEN_MULTISTEP = 'sharpen.multistep';
+    const SHARPEN_STRONG = 'sharpen.strong';
+    const SHARPEN_MEDIUM = 'sharpen.medium';
+    const SHARPEN_SOFT = 'sharpen.soft';
+
+    private $type = null;
+    private static $Supported = array(
+        self::SHARPEN_MULTISTEP => array(
+            array(-1.2, -1, -1.2),
+            array(-1, 20, -1),
+            array(-1.2, -1, -1.2)
+        ),
+        self::SHARPEN_STRONG => array(
+            array(-1.4, -1.2, -1.4),
+            array(-1.2, 16, -1.2),
+            array(-1.4, -1.2, -1.4)
+        ),
+        self::SHARPEN_MEDIUM => array(
+            array(-1, -1, -1),
+            array(-1, 16, -1),
+            array(-1, -1, -1)
+        ),
+        self::SHARPEN_SOFT => array(
+            array(-1, -1, -1),
+            array(-1, 20, -1),
+            array(-1, -1, -1)
+        )
+    );
+
+    /**
+     * Constrcut new sharpen action
+     *
+     * @param string $type
+     */
+    public function __construct($type = self::SHARPEN_MULTISTEP)
+    {
+        $this->setType($type);
+    }
+
+    /**
+     * Set sharpen type
+     *
+     * @param string $type
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function setType($type)
+    {
+        if (!array_key_exists($type, self::$Supported)) {
+            throw new \InvalidArgumentException(sprintf(
+                    'Invalid Sharpen Type "%s"', $type
+            ));
+        }
+        $this->type = $type;
+    }
+
+    /**
+     * Get sharpen type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
     /**
      * {@inheritdoc}
      */
     protected function doApply(CanvasInterface $canvas)
     {
-        $con = new Convolution(array(
-            array(-1, -1, -1)
-            , array(-1, 9, -1)
-            , array(-1, -1, -1)
-        ), 1.0);
+        $matrix = self::$Supported[$this->getType()];
+        $con = new Convolution(
+                $matrix
+                , array_sum(array_map('array_sum', $matrix))
+        );
         $con->apply($canvas);
     }
 
